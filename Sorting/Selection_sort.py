ead835aa0d5870e40b5bb06f4c0952798fc1d471
Sumangiri123 <sumangiri1964@gmail.com>
2024-04-01 01:11:40 +0530

    Selection Sort Algorithm

--- Sorting/Selection_sort.py
+++ Sorting/Selection_sort.py
@@ -1,15 +1,21 @@
+# Define a function named 'Selection_sort' that takes a list 'arr' as input.
 def Selection_sort(arr):
+    # Iterate through each index in the list 'arr'.
     for i in range(len(arr)):
+        # Assume the current index is the minimum.
         min = i
-        for j in range(i+1,len(arr)):
+        # Iterate through the rest of the list starting from the next index.
+        for j in range(i+1, len(arr)):
+            # If a smaller element is found, update the minimum index.
             if arr[j] < arr[min]:
                 min = j
-        arr[i],arr[min] = arr[min],arr[i]
-
+        # Swap the element at the current index with the smallest element found.
+        arr[i], arr[min] = arr[min], arr[i]
+    # Return the sorted list.
     return arr
 
-    
+
 # Initialize an array with unsorted elements.
 arr = [5,8,1,6,2,4,3]
-# Print the sorted array by calling the 'Bubble_sort' function.
+# Print the sorted array by calling the 'Selection_sort' function.
 print(Selection_sort(arr))
