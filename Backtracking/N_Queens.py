50ca7bc56b9f51b0cd6d71eb342f8af09ff1d8f2
Sumangiri123 <sumangiri1964@gmail.com>
2024-04-09 13:42:37 +0530

    N-Queens

--- Backtracking/N_Queens.py
+++ Backtracking/N_Queens.py
@@ -1,31 +1,60 @@
-def issafetoplace(board,row,col,n):
-
-    # checking left side 
-    for i in range(n):
+def issafetoplace(board, row, col, n):
+    # Checking left side
+    for i in range(col):
         if board[row][i] == 1:
             return False
-        
-    # checking upper left diagonal
-    for i,j in zip(range(row,-1,-1),range(col,-1,-1)):
+
+    # Checking upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # checking lower left diagonal
-    for i,j in zip(range(row,n,1),range(col,-1,-1)):
+    # Checking lower left diagonal
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
     return True
 
-def solveNQ(board,n):
-    if not solveNQutil(board,0,n):
+def solveNQutil(board, col, n):
+    if col >= n:
+        return True
+
+    for row in range(n):
+        if issafetoplace(board, row, col, n):
+            board[row][col] = 1 # Place a queen at the current position
+
+            if solveNQutil(board, col + 1, n):
+                return True
+
+            board[row][col] = 0 # Backtrack if no solution is found
+
+    return False
+
+def solveNQ(board, n):
+    if not solveNQutil(board, 0, n):
         print("Solution doesn't exist")
         return
-    
-    printBoard(board,n)
 
-def solveNQutil(board,col,n):
-    if col >= n:
-        return
+    printBoard(board, n)
 
+def printBoard(board, n):
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 1:
+                print("Q", end="")
+            else:
+                print(".", end="")
+        print()
+
+board = [[0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]]
+
+solveNQ(board, 4)
+
+
+# Time Complexity :
+# Space Complexity :
     
